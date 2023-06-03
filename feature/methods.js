2796f75da23830d0e252a257a4c2ad6d8de18b3f
Quinn <quinn@indra.ai>
2023-06-03 14:00:29 -0700

    Update deva.world core to latest version

--- feature/methods.js
+++ feature/methods.js
@@ -1,5 +1,36 @@
 module.exports = {
   /**************
+  method: service
+  params: packet
+  describe: The global service feature that installs with every agent
+  ***************/
+  services(packet) {
+    const services = this.services();
+    const data = {};
+    return new Promise((resolve, reject) => {
+      this.question(`#docs raw feature/services`).then(doc => {
+        data.doc = doc.a.data;
+        const info = [
+          `## Settings`,
+          `::begin:services:${services.id}`,
+          `client: ${services.client_name}`,
+          `concerns: ${services.concerns.join(', ')}`,
+          `::end:services:${this.hash(services)}`,
+        ].join('\n');
+        const text = doc.a.text.replace(/::info::/g, info)
+        return this.question(`#feecting parse ${text}`)
+      }).then(feecting => {
+        return resolve({
+          text: feecting.a.text,
+          html: feecting.a.html,
+          data: services
+        });
+      }).catch(err => {
+        return this.error(err, packet, reject);
+      })
+    });
+  },
+  /**************
   method: uid
   params: packet
   describe: Return a system id to the user from the Log Buddy.
@@ -8,60 +39,4 @@ module.exports = {
     this.context('uid');
     return Promise.resolve(this.uid());
   },
-  /**************
-  method: status
-  params: packet
-  describe: Return the current status of the Log Buddy.
-  ***************/
-  status(packet) {
-    this.context('status');
-    return Promise.resolve(this.status());
-  },
-  /**************
-  method: info
-  params: packet
-  describe: Return the current info for the deva.
-  ***************/
-  info(packet) {
-    return Promise.resolve(this.info);
-  },
-  /**************
-  method: issue
-  params: packet
-  describe: create a new issue for the main deva.world through github agent.
-  ***************/
-  issue(packet) {
-    return new Promise((resolve, reject) => {
-      this.question(`#github create_issue:${packet.q.agent.key} ${packet.q.text}`).then(issue => {
-        return resolve({
-          text: issue.a.text,
-          html: issue.a.html,
-          data: issue.a.data,
-        })
-      }).catch(err => {
-        return this.error(err, packet, reject);
-      });
-    });
-  },
-  /**************
-  method: help
-  params: packet
-  describe: The Help method returns the information on how to use the Log Buddy.
-  ***************/
-  help(packet) {
-    this.context('help');
-    const {dir} = this.info();
-    console.log('INFO', this.info());
-    return new Promise((resolve, reject) => {
-      this.help(packet.q.text, dir).then(help => {
-        return this.question(`#feecting parse ${help}`);
-      }).then(parsed => {
-        return resolve({
-          text: parsed.a.text,
-          html: parsed.a.html,
-          data: parsed.a.data,
-        });
-      }).catch(reject);
-    });
-  }
 };
