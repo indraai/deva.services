caacdfc364167685fa00a3d913872d5c00f1a2fe
Quinn Michaels <quinn@indra.ai>
2025-05-01 15:33:09 -0700

    update deva core and package profile

--- feature/methods.js
+++ feature/methods.js
@@ -20,11 +20,13 @@ export default {
       const agent = this.agent();
       const global = [];
       services.global.forEach((item,index) => {
-        global.push(`::begin:global:${item.key}:${item.id}`);
+        global.push(`::begin:${item.key}:${item.id}`);
         for (let x in item) {
           global.push(`${x}: ${item[x]}`);
         }
-        global.push(`::end:global:${item.key}:${this.lib.hash(item)}`);
+        const thehash = this.lib.hash(item);
+        global.push(`hash: ${thehash}`);
+        global.push(`::end:${item.key}:${thehash}`);
       });
       const concerns = [];
       services.concerns.forEach((item, index) => {
@@ -33,16 +35,20 @@ export default {
       
       const info = [
         '::BEGIN:SERVICES',
-        '### Client',
-        `::begin:client:${services.client_id}`,
+        `::begin:client`,
+        '## Client',
         `id: ${services.client_id}`,
         `client: ${services.client_name}`,
-        '**concerns**',
-        concerns.join('\n'),
-        `::end:client:${this.lib.hash(services)}`,
-        '### Global',
+        `::end:client}`,
+        concerns.length ? `::begin:concerns` : '',
+        concerns.length ? '## Concerns' : '',
+        concerns.length ? concerns.join('\n') : '',
+        concerns.length ? `::end:concerns` : '',
+        '::begin:global',
+        '## Global',
         global.join('\n'),
-        '::END:SERVICES'
+        '::end:global',
+        '::END:SERVICES',
       ].join('\n');
       this.question(`${this.askChr}feecting parse ${info}`).then(feecting => {
         return resolve({
