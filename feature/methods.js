c5f848645ba90799a22a58197da01e56044ea0d9
Quinn Michaels <quinn@indra.ai>
2026-02-17 13:05:37 -0800

    Update Services Deva to Deva Core Quad-Date Triple-Hash standard.

--- feature/methods.js
+++ feature/methods.js
@@ -3,15 +3,6 @@
 // Legal Signature Required For Lawful Use.
 // Distributed under VLA:59232188337076320338 LICENSE.md
 
-function buildProfile(entity, type='assistant') {
-  const _profile = [];
-  _profile.push(`::begin:${type}:${entity.id}`);
-  for (let x in entity.profile) {
-    _profile.push(`${x}: ${entity.profile[x]}`);
-  }
-  _profile.push(`::end:${type}:${entity.hash}\n`);
-  return _profile.join('\n');
-}
 export default {
   /**************
   method: service
@@ -92,14 +83,9 @@ export default {
       let doc = false;
       try {
         const doc_file = this.lib.fs.readFileSync(docPath, 'utf8');  
-        const doc_content = doc_file.split(`::BEGIN:${part}`)[1].split(`::END:${part}`)[0];
-        doc = [
-          `${this.container.begin}:FILE:${packet.id.uid}`,
-          doc_content,
-          `${this.container.end}:FILE:${packet.id.uid}`,          
-        ].join('\n');
+        doc = doc_file.split(`::BEGIN:${part}`)[1].split(`::END:${part}`)[0];
       } catch (err) {
-        console.log(err);
+        return this.err(err, packet, reject);
       }
             
       this.question(`${this.askChr}feecting parse ${doc}`, {vars:this.vars}).then(feecting => {
@@ -111,83 +97,8 @@ export default {
         });
       }).catch(err => {
         this.state('reject', `file:${packet.q.text}`);
-        return this.error(err, packet, reject);
+        return this.err(err, packet, reject);
       })
     });
   },    
-
-  /**************
-  method: ask
-  params: packet
-  describe: this method provides a global ask method to all agents.
-  ***************/
-  async ask(packet) {
-    this.zone('services', packet.id.uid);
-    this.feature('services', packet.id.uid);
-    this.context('ask', `${packet.q.agent.name}:${packet.id.uid}`);
-    this.action('services', `ask:${packet.id.uid}`);
-    this.state('services', `ask:${packet.id.uid}`);
-    const data = {};
-    const agent = this.agent();
-    const client = this.client();
-    const info = this.info();
-
-    this.state('get', `corpus:${packet.id.uid}`);
-    const _corpus = await this.help('corpus', info.dir);
-    const corpus = await this.question(`${this.askChr}feecting parse ${_corpus}`);
-
-    return new Promise((resolve, reject) => {
-      if (!this.vars.ask) return resolve('Ask not configured.');
-
-      data.corpus = corpus.a.text;
-
-      this.state('set', `history:${packet.id.uid}`);
-      this.vars.ask.history.push({
-        role: 'user',
-        content: packet.q.text,
-      });
-
-      this.state('get', `ask:chat:${packet.id.uid}`);
-      this.question(`${this.askChr}chat relay ${decodeURIComponent(packet.q.text)}`, {
-        client: buildProfile(client, 'client'),
-        agent: buildProfile(agent, 'agent'),
-        corpus: corpus.a.text,
-        max_tokens: this.vars.ask.max_tokens,
-        history: this.vars.ask.history.slice(-10),
-        memory: agent.key,
-      }).then(answer => {
-
-        data.chat = answer.a.data.chat;  
-        const text = [
-          `::begin:${agent.key}:${answer.id.uid}`,
-          answer.a.text,
-          `date: ${this.lib.formatDate(answer.a.created, 'long', true)}`,
-          `::end:${agent.key}:${answer.id.uid}`,
-        ];
-        this.state('set', `history:a:${packet.id}`);
-        this.vars.ask.history.push({
-          role: answer.a.data.chat.role,
-          content: answer.a.data.chat.text,
-        });
-  
-        this.state('parse', `ask:${packet.id.uid}`);
-        return this.question(`${this.askChr}feecting parse ${text.join('\n')}`);
-      }).then(feecting => {
-        data.feecting = feecting.a.data;
-        this.action('resolve', `ask:${packet.id.uid}`);
-        this.state('valid', `ask:${packet.id.uid}`);
-        this.intent('good', `ask:${packet.id.uid}`);
-        return resolve({
-          text: feecting.a.text,
-          html: feecting.a.html,
-          data,
-        });
-      }).catch(err => {
-        this.action('reject', 'chat');
-        this.state('invalid', 'chat');
-        this.intent('bad', 'chat');
-        return this.err(packet, err, reject);
-      });
-    });
-  },
 };
